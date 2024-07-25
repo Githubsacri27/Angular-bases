a97b56b4cc9359b75852ef371281deaf87f6b391
Rubén <rsacri27@hotmail.com>
2024-07-25 11:28:50 +0200

    service,uuid

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -6,11 +6,20 @@ import { Character } from '../../interfaces/character.interface';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent { 
-  
+export class ListComponent {
   @Input()
-  public characterList: Character[] =[{
-    name: 'Trunks',
-    power: 100
-  }]
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 100,
+    },
+  ];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    this.onDelete.emit(id);
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,26 +1,24 @@
 import { Component } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 import { Character } from '../interfaces/character.interface';
+
 @Component({
     selector: 'app-dbz-main-page',
     templateUrl: './main-page.component.html'
 })
 
 export class MainPageComponent{
+    constructor(private dbzService: DbzService){}
 
-    public characters: Character[] =[{
-        name: 'Krillin',
-        power: 1000
-    },{
-        name: 'Goku',
-        power: 9500
-    },
-    {
-        name: 'Vegeta',
-        power: 7500
-    }];
+    get characters(): Character[]{
+        return [...this.dbzService.characters];
+    }
 
-    onNewCharacter(character: Character): void{
-        console.log('MainPage');
-        console.log(character);
+    onDeleteCharacter(id: string):void{
+        this.dbzService.onDeleteCharacterById(id);
+    }
+
+    onNewCharacter(character: Character):void{
+        this.dbzService.addCharacter(character);
     }
 }
